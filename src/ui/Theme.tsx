4fb78575c708a2d4e4d6b629c557fb023e48a8fb
Hayden Young <hi@hbjy.dev>
2021-04-10 11:20:07 +0100

    chore: prettier

--- src/ui/Theme.tsx
+++ src/ui/Theme.tsx
@@ -1,38 +1,40 @@
 import React, { createContext } from 'react';
 
 export interface ThemeColors {
-    light: string[];
-    primary: string[];
-    success: string[];
-    danger: string[];
-    warning: string[];
-    info: string[];
-    like: string[];
-};
+  light: string[];
+  primary: string[];
+  success: string[];
+  danger: string[];
+  warning: string[];
+  info: string[];
+  like: string[];
+}
 
 type Theme = {
-    colors: ThemeColors
+  colors: ThemeColors;
 };
 
 const defaultTheme = {
-    colors: {
-        light: ['white', 'gray-900', 'gray-200'],
-        primary: ['green-500', 'white', 'green-200'],
-        success: ['green-500', 'white', 'green-200'],
-        danger: ['red-500', 'white', 'red-200'],
-        warning: ['yellow-500', 'white', 'yellow-200'],
-        info: ['blue-500', 'white', 'blue-200'],
-        like: ['pink-500', 'white', 'pink-200']
-    } as ThemeColors
+  colors: {
+    light: ['white', 'gray-900', 'gray-200'],
+    primary: ['green-500', 'white', 'green-200'],
+    success: ['green-500', 'white', 'green-200'],
+    danger: ['red-500', 'white', 'red-200'],
+    warning: ['yellow-500', 'white', 'yellow-200'],
+    info: ['blue-500', 'white', 'blue-200'],
+    like: ['pink-500', 'white', 'pink-200'],
+  } as ThemeColors,
 } as Theme;
 
 const ThemeContext = createContext(defaultTheme);
 
 type Props = { children: React.ReactNode };
 export const ThemeProvider = ({ children }: Props) => {
-    return (
-        <ThemeContext.Provider value={defaultTheme}>{children}</ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={defaultTheme}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => React.useContext(ThemeContext);
